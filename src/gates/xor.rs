c134893de621441df0cc208496583f618f9cf103
Joakim Bergros <jbv87@protonmail.com>
2024-06-22 14:38:17 +0200

    Changed to using my own gates

--- src/gates/xor.rs
+++ src/gates/xor.rs
@@ -1,3 +1,5 @@
+use super::{nand, not, or};
+
 /// Returns true if only one input is true
 ///
 /// # Truth table
@@ -27,7 +29,9 @@
 /// assert_eq!(should_be_false, false);
 /// ```
 pub fn xor(left: bool, right: bool) -> bool {
-    left != right
+    nand(or(left, not(right)), or(right, not(left)))
+
+    // left != right
 }
 
 #[cfg(test)]
@@ -35,7 +39,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn xor_false_when_both_are_true() {
+    fn xor_false_when_both_are_same() {
         assert_eq!(xor(true, true), false);
+        assert_eq!(xor(false, false), false);
+    }
+
+    #[test]
+    fn xor_true_when_only_one_are_true() {
+        assert_eq!(xor(false, true), true);
+        assert_eq!(xor(true, false), true);
     }
 }
